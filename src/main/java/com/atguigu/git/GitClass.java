37bd0dfd26bd87cff16c17fc9512691b51a8feec
3s <289334243@qq.com>
2022-08-21 10:34:37 +0800

    hot-fix second commit

--- src/main/java/com/atguigu/git/GitClass.java
+++ src/main/java/com/atguigu/git/GitClass.java
@@ -4,5 +4,6 @@ public class GitClass {
     public static void main(String[] args) {
         System.out.println("hello world1");
         System.out.println("hot-fix:hello world!");
+        System.out.println("hot-fix:hello World1");
     }
 }
