f210e54c74442f724bfbbcfe65a54ae23853b5a0
3s <289334243@qq.com>
2022-08-21 10:32:25 +0800

    hot-fix first commit

--- src/main/java/com/atguigu/git/GitClass.java
+++ src/main/java/com/atguigu/git/GitClass.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitClass {
     public static void main(String[] args) {
         System.out.println("hello world1");
+        System.out.println("hot-fix:hello world!");
     }
 }
