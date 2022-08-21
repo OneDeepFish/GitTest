87a31f2b29887d66f842c70574d90232f71b0b18
OneDeepFish <55479984+OneDeepFish@users.noreply.github.com>
2022-08-21 11:58:13 +0800

    修改代码

--- src/main/java/com/atguigu/git/GitTestClass.java
+++ src/main/java/com/atguigu/git/GitTestClass.java
@@ -2,6 +2,6 @@ package com.atguigu.git;
 
 public class GitTestClass {
     public static void main(String[] args) {
-        System.out.println("nidaye");
+        System.out.println("I have a dream!");
     }
 }
